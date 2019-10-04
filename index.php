1c467110de5ac0aa6c2472a2279542ca2edde65e
owlda <e1895226@cmaisonneuve.qc.ca>
2019-10-04 15:38:27 -0400

    initial

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+<h1>Welcome</h1>
+</body>
+</html>
+<?php
