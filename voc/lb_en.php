01ef95740cb501c73d23315b42212ce5f4ce7b66
Ivan Dziuba <kipivip@gmail.com>
2019-12-09 16:46:11 -0500

    add files Ivan

---
+++ voc/lb_en.php
@@ -0,0 +1,17 @@
+<?php
+
+ $tmp = array();
+
+ $tmp['email'] = "dhsgsadjhasgd:";
+$tmp = array();
+
+$tmp["label_titre_circuit"] = "fdsfsdf:";
+$tmp["label_description_circuit"] = "fsdfsdf:";
+$tmp["label_theme_circuit"] = "fdsf:";
+$tmp["btn_add_circuit"] = "fdsfsdf";
+
+$tmp["theme_circuit"]["50"] = "1111";
+$tmp["theme_circuit"]["40"] = "2222";
+$tmp["theme_circuit"]["2"] = "3333";
+
+?>
---
+++ voc/lb_fr.php
@@ -0,0 +1,18 @@
+<?php
+$tmp = array();
+$tmp['email'] = "Corriel:";
+$tmp["label_titre_circuit"] = "Titre:";
+$tmp["label_description_circuit"] = "Description:";
+$tmp["label_theme_circuit"] = "Thème:";
+$tmp["btn_add_circuit"] = "Ajouter";
+
+//TODO Get from DB
+$tmp["theme_circuit"] = array();
+
+$tmp["theme_circuit"]["50"] = "Historique";
+$tmp["theme_circuit"]["40"] = "Gastronomique";
+$tmp["theme_circuit"]["2"] = "Ethnographique";
+
+
+
+?>
