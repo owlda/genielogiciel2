971e7459f2f807ee6787937d9baa04a72c37948d
owlda <e1895226@cmaisonneuve.qc.ca>
2019-12-09 22:31:21 -0500

    index pages fixed

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,10 +1,10 @@
 <?php require APPROOT.'/views/_layouts/headerApp.php'?>
 <body>
 <?php require APPROOT.'/views/_layouts/navbar.php'?>
-<h3 id="allFilms">TOUS LES FILMS & SÉRIES</h3>
+<h3 id="allFilms">INDEX IN PAGES</h3>
 <body>
 <div>
-    <?php include(APPROOT.'/functions/readall.php');?>
+<p>Some content here...</p>
 </div>
 </body>
 
