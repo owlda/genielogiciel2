5f164e2d9f7483a35a36165f7a1ddc1d24074121
Vahe Hayrapetyan <vahikoh@gmail.com>
2019-12-18 22:57:39 -0500

    Connexion d'un client existant

--- ControllerVahe/ViewVahe.js
+++ ControllerVahe/ViewVahe.js
@@ -5,5 +5,20 @@ var membreVue = function(reponse){
             if(reponse.msg === 'NOK')
                 alert("Les mot de passe ne correspondent pas");
             break;
+        case 'connecter':
+            if(reponse.msg=='BADPASS')
+            {
+                alert('Mauvais mot de passe ou courriel')
+            }
+            else if(reponse.msg == 'NOTMEMBER')
+            {
+                alert('Vous n\'étes pas membre');
+            }
+            else
+            {
+                // si le requete marche et le mot de passe est correct on dirige vers le site du client
+                window.location.href = '../index_ren.php';
+            }
+            break;
     }
 }
--- ControllerVahe/requetsMembre.js
+++ ControllerVahe/requetsMembre.js
@@ -14,8 +14,30 @@ function requetEnregistrerMembre() {
             membreVue(reponse);
         },
         error: function (xhr, error) {
-            alert('L\'ecution du script n\'est pas reussit');
+            alert('L\'execution du script n\'est pas reussit');
         }
     });
 
+}
+
+// la fonction pour faire POST par ajax pour envoyer les données de la formuler de la login vers le serveur
+function connecter()
+{
+    var formConnexion = new FormData(document.getElementById('formConnexion'));
+    formConnexion.append('action','connecter');
+    $.ajax({
+        type : 'POST',
+        url : '../ControllerVahe/membreController.php',
+        data : formConnexion,
+        contentType : false,
+        processData : false,
+        dataType : 'json',
+        success : function (reponse){
+
+            membreVue(reponse);
+        },
+        error: function (xhr, error) {
+            alert('Erreur de execution du script');
+        }
+    });
 }
