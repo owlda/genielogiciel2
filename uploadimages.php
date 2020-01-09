cc4a7666ed28f08c12c4dc47de41150301e8e393
owlda <e1895226@cmaisonneuve.qc.ca>
2020-01-09 13:42:37 -0500

    upload images added

--- uploadimages.php
+++ uploadimages.php
@@ -1,11 +1,26 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html>
 <?php include __DIR__ . '/tmp/template/headerDenis.tpl ' ?>
 <body>
-<p style="margin-top: 5%; margin-bottom: 5%; text-align: center">Template 'uploadimgs.tpl ' pour sauvegarder les images dans le repertoire 'upload' pour circuits.</p>
+<p style="margin-top: 5%; margin-bottom: 5%; text-align: center">Ajouter les images:</p>
+
 <?php include __DIR__ . '/tmp/template/uploadimgs.tpl ' ?>
 </body>
 </html>
+<?php
+if (isset($_POST['submitN'])) {
 
+    try {
+
+        $num_circuit = $_POST['idNumeroC'];
+        $mkdir = @mkdir('upload/'.$num_circuit);
+        $_SESSION['numCircuit'] = $num_circuit;
+
+    }
+    catch (Exception $e) {
+        print $e->getMessage();
+    }
+}
 
 
