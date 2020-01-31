7476a05ad79c0e6b01c6d9c48f0bec9cde770b82
Ruben <ruben246>
2020-01-31 16:22:42 -0500

    Connexion avec Paypal - Amélioration de l'exemple de panier de réservation, qui affiche les boutons PayPal

--- ControllerVahe/ViewVahe.js
+++ ControllerVahe/ViewVahe.js
@@ -1,26 +1,19 @@
-var membreVue = function(reponse){
-    switch(reponse.action)
-    {
+var membreVue = function (reponse) {
+    switch (reponse.action) {
         case 'devenirmembre':
-            if(reponse.msg === 'NOK')
+            if (reponse.msg === 'NOK')
                 alert("Les mot de passe ne correspondent pas");
-            if(reponse.msg == 'OK')
-            {
+            if (reponse.msg == 'OK') {
                 alert('sakjdhajfhk');
                 window.location.href = '../index_ren.php';
             }
             break;
         case 'connecter':
-            if(reponse.msg=='BADPASS')
-            {
+            if (reponse.msg == 'BADPASS') {
                 alert('Mauvais mot de passe ou courriel')
-            }
-            else if(reponse.msg == 'NOTMEMBER')
-            {
+            } else if (reponse.msg == 'NOTMEMBER') {
                 alert('Vous n\'étes pas membre');
-            }
-            else
-            {
+            } else {
                 // si le requete marche et le mot de passe est correct on dirige vers le site du client
                 window.location.href = '../index_client_vahe.php';
             }
