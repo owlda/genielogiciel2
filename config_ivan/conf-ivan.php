d28ede12302a35bf964a5b7412a1764621104c98
owlda <e1895226@cmaisonneuve.qc.ca>
2020-02-03 12:26:23 -0500

    ini comment

--- config_ivan/conf-ivan.php
+++ config_ivan/conf-ivan.php
@@ -7,7 +7,7 @@ $db->port = 3306;
 $server = "localhost";
 $user = "root";
 $password = "";
-$database = "northernstartravelv";
+$database = "northernstartravel";
 $db->connect($server, $user, $password, $database);
 
 //TODO Initialization Smarty
---
+++ tmp/configs/8c0d084693db957f4cd3590424afffb0285cb9e6_0.file.modal_del_circuit.tpl.php
@@ -0,0 +1,46 @@
+<?php
+/* Smarty version 3.1.33, created on 2020-01-31 21:26:56
+  from 'C:\web\xampp\htdocs\genielogiciel2-master\tmp\template\modal_del_circuit.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5e348d903f3225_03397032',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '8c0d084693db957f4cd3590424afffb0285cb9e6' => 
+    array (
+      0 => 'C:\\web\\xampp\\htdocs\\genielogiciel2-master\\tmp\\template\\modal_del_circuit.tpl',
+      1 => 1579704691,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5e348d903f3225_03397032 (Smarty_Internal_Template $_smarty_tpl) {
+?><!-- Modal -->
+<div class="modal fade" id="modal_delete" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Suppimer le circuit?</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                "<?php echo $_smarty_tpl->tpl_vars['titre']->value;?>
+" va être supprimé...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+                <button type="button" class="btn btn-danger">Supprimer</button>
+            </div>
+        </div>
+    </div>
+</div>
+<?php }
+}
